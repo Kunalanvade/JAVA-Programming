ec5e4c41785f1df91491842350ab39939f879a73
Kunalanvade <129099600+Kunalanvade@users.noreply.github.com>
2024-02-01 01:45:40 +0530

    about function and method

---
+++ Functions/hey.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class hey {
+
+    public static void print_hey() {
+        System.out.println("Hello Worlds");
+    }
+
+    public static void main(String[] args) {
+        print_hey();
+    }
+}
