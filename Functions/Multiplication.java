77687ab57bfa73495e3bf696f3ca3d8bd7546084
Kunalanvade <129099600+Kunalanvade@users.noreply.github.com>
2024-02-02 00:59:01 +0530

    product of two  numbers !!

---
+++ Functions/Multiplication.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Multiplication {
+
+    public static int multiply(int a, int b) {
+        int product = a * b;
+        return product;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int prod = multiply(a, b);
+        System.out.println(prod);
+
+    }
+
+}
