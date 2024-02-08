b24145b6f050ee5f46a436440485670d8d6fad88
Kunalanvade <129099600+Kunalanvade@users.noreply.github.com>
2024-02-09 01:44:51 +0530

    solid Rhombus

---
+++ Advance_pattern/Solid_Rhombus.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solid_Rhombus {
+
+    public static void rhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            // for spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // for stars
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        rhombus(8);
+    }
+}
